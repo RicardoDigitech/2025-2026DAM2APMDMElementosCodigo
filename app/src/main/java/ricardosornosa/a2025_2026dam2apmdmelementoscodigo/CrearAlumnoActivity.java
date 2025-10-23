7014b31226eb73e8cce72c2f55d604ce0317ba8d
RicardoDigitech <ricardo.sornosa@digitechfp.com>
2025-10-23 11:17:26 +0200

    Finalización.

--- app/src/main/java/ricardosornosa/a2025_2026dam2apmdmelementoscodigo/CrearAlumnoActivity.java
+++ app/src/main/java/ricardosornosa/a2025_2026dam2apmdmelementoscodigo/CrearAlumnoActivity.java
@@ -1,6 +1,9 @@
 package ricardosornosa.a2025_2026dam2apmdmelementoscodigo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import ricardosornosa.a2025_2026dam2apmdmelementoscodigo.databinding.ActivityCrearAlumnoBinding;
+import ricardosornosa.a2025_2026dam2apmdmelementoscodigo.models.AlumnoModel;
 
 public class CrearAlumnoActivity extends AppCompatActivity {
     private ActivityCrearAlumnoBinding binding;
@@ -20,5 +24,69 @@ public class CrearAlumnoActivity extends AppCompatActivity {
                 ActivityCrearAlumnoBinding
                         .inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        binding.btnCancelarCrearAlumno
+                .setOnClickListener(
+                        new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                setResult(RESULT_CANCELED);
+                                finish();
+                            }
+                        });
+
+        binding.btnCrearCrearAlumno
+                .setOnClickListener(
+                        new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                AlumnoModel a = crearAlumno();
+                                if (a != null) {
+                                    Intent intent = new Intent();
+                                    Bundle bundle = new Bundle();
+                                    bundle.putSerializable(
+                                            "AL", a
+                                    );
+                                    intent.putExtras(bundle);
+                                    setResult(RESULT_OK, intent);
+                                    finish();
+                                }
+                            }
+                        });
+    }
+
+    private AlumnoModel crearAlumno() {
+        if (binding.txtNombreCrearAlumno.getText()
+                .toString().isEmpty() &&
+                binding.txtApellidosCrearAlumno.getText()
+                        .toString().isEmpty()) {
+            return null;
+        }
+
+        if (binding.spCicloCrearAlumno
+                .getSelectedItemPosition() == 0) {
+            return null;
+        }
+
+        if (!binding.rbGrupoACrearAlumno.isChecked()
+                && !binding.rbGrupoBCrearAlumno.isChecked()
+                && !binding.rbGrupoCCrearAlumno.isChecked()) {
+            return null;
+        }
+
+        String nombre = binding.txtNombreCrearAlumno
+                .getText().toString();
+        String apellido = binding.txtApellidosCrearAlumno
+                .getText().toString();
+        String ciclo = (String) binding.spCicloCrearAlumno
+                .getSelectedItem();
+        RadioButton rb =
+                findViewById(binding.rgGrupoCrearAlumno
+                        .getCheckedRadioButtonId());
+        char grupo = rb.getText().charAt(6);
+        AlumnoModel a = new AlumnoModel(
+                nombre, apellido, ciclo, grupo
+        );
+        return a;
     }
 }
--- app/src/main/java/ricardosornosa/a2025_2026dam2apmdmelementoscodigo/MainActivity.java
+++ app/src/main/java/ricardosornosa/a2025_2026dam2apmdmelementoscodigo/MainActivity.java
@@ -1,15 +1,27 @@
 package ricardosornosa.a2025_2026dam2apmdmelementoscodigo;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import ricardosornosa.a2025_2026dam2apmdmelementoscodigo.databinding.ActivityMainBinding;
+import ricardosornosa.a2025_2026dam2apmdmelementoscodigo.models.AlumnoModel;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    private ActivityResultLauncher<Intent> cALauncher;
+    private ArrayList<AlumnoModel> aList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +30,63 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         setSupportActionBar(binding.toolbar);
 
+        aList = new ArrayList<>();
+        inicializarLaunchers();
+
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(
+                        MainActivity.this,
+                        CrearAlumnoActivity.class
+                );
+                cALauncher.launch(intent);
             }
         });
+    }
+
+    private void inicializarLaunchers() {
+        cALauncher = registerForActivityResult(
+                new ActivityResultContracts
+                        .StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult o) {
+                        if (o.getResultCode() == RESULT_OK) {
+                            if (o.getData() != null) {
+                                Intent intent = o.getData();
+                                Bundle bundle = intent.getExtras();
+                                AlumnoModel a = (AlumnoModel) bundle.getSerializable(
+                                        "AL"
+                                );
+                                if (a != null) {
+                                    aList.add(a);
+                                    mostrarAlumnos();
+                                }
+                            }
+                        }
+                    }
+                });
+    }
+
+    private void mostrarAlumnos() {
+        binding.contentMain.contenedorMain.removeAllViews();
+        for (AlumnoModel a : aList) {
+            LayoutInflater li = LayoutInflater.from(MainActivity.this);
+            View alumnoView = li.inflate(R.layout.alumno_model_view, null);
+            TextView lblNombre =
+                    alumnoView.findViewById(R.id.lblNombreAlumnoView);
+            TextView lblApellidos =
+                    alumnoView.findViewById(R.id.lblApellidosAlumnoView);
+            TextView lblCiclo =
+                    alumnoView.findViewById(R.id.lblCicloAlumnoView);
+            TextView lblGrupo =
+                    alumnoView.findViewById(R.id.lblGrupoAlumnoView);
+            lblNombre.setText(a.getNombre());
+            lblApellidos.setText(a.getApellidos());
+            lblCiclo.setText(a.getCiclo());
+            lblGrupo.setText(String.valueOf(a.getGrupo()));
+            binding.contentMain.contenedorMain.addView(alumnoView);
+        }
     }
 }
